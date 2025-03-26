a239c98de6eab9dc9cf3a26b0d22dcad2d0f22c1
luisso <luisotaviorsilva@hotmail.com>
2025-03-26 13:14:02 -0300

    pacote_teste curso DIO

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pacote_simples
- 
+
+Description.
+The package pacote_simples is used to:
+    package_name:
+        file1_name
+            - Template course DIO 
+        file2_name
+            - Template course DIO
+
+## Installation
+```bash
+pip install package_name
+```
+
+## Author
+Luís Otávio Rodrigues da Silva
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    author="Luís Otávio Rodrigues da Silva",
+    author_email="luisotaviorsilva@hotmail.com",
+    description="Pacote teste do Bootcamp Python Developer",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
